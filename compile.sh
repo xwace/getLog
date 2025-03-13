dddf70f6d34433b48c267c448f1b855aff97c15f
xwace <37037519+xwace@users.noreply.github.com>
2025-03-13 10:18:47 +0800

    Create compile.sh
    搜索所有可用ip

---
+++ compile.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/expect
+
+set ip_head 10.10.35.
+
+#搜索所有网域内可用的ip
+# for {set i 151} {$i < 255} {incr i} {
+#     spawn ssh root@$ip_head$i
+#     expect "password"
+# }
+
+spawn sh -c {
+    cd build
+    echo "\e[36m===choose platform===: \e[0m"
+    # read input
+    cmake .. -DCHOOSE_PLATFORM=1 #$input
+    make -j32 
+    ./BFStest }
+
+interact
+sleep 20000
+
+# spawn scp ./build/BFStest root@$ip:/home/robot/business/log
+# expect "password"
+# send "yx113322\r"
+
+# spawn ssh root@$ip
+# expect "password:"
+# send "yx113322\r"
+# expect "#"
+# send "cd /home/robot/business/log\r"
+# send "./BFStest\r"
+# send "rm BFStest;exit"
+# interact
